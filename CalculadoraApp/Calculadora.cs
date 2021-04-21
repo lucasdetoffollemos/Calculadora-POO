b02f1168478e0dd0cba1fc40e0249ba44b9330c7
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2021-04-21 01:43:44 -0300

    Código igual o da outra calculadora, só que agora programado em orientação a objeto

---
+++ CalculadoraApp/Calculadora.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraApp
+{
+    class Calculadora
+    {
+        public double numero1, numero2;
+        public string operacao;
+        public string [] resultados = new string [100];
+
+        public void MostraMenu()
+        {
+            Console.WriteLine("Calculadora ");
+            Console.WriteLine("Digite 1 para somar: ");
+            Console.WriteLine("Digite 2 para subtrair: ");
+            Console.WriteLine("Digite 3 para multiplicar: ");
+            Console.WriteLine("Digite 4 para dividir: ");
+            Console.WriteLine("Digite 5 para ver as operações: ");
+            Console.WriteLine("Digite S para sair: ");
+        }
+
+        public bool VerificaOpcaoInvalida(string opcao)
+        {
+            bool opcaoInvalida = opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "5" && opcao != "s" && opcao != "S";
+            return opcaoInvalida;
+        }
+
+        public void MostraSimboloOperacao(string op)
+        {
+            if(op == "1")
+                operacao = "+";
+            
+            else if (op == "2")
+                operacao = "-";
+            
+            else if (op == "3")
+                operacao = "*";
+         
+            else if (op == "4")
+                operacao = "/";
+        }
+
+        public double RealizaCalculo(string op, double n1, double n2)
+        {
+            double resultado = 0;
+
+            switch (op)
+            {
+                case "1":
+                    resultado = n1 + n2;
+                    break;
+                case "2":
+                    resultado = n1 - n2;
+                    break;
+                case "3":
+                    resultado = n1 * n2;
+                    break;
+                case "4":
+                    resultado = n1 / n2;
+                    break;
+                default: break;
+            }
+
+            return resultado;
+        }
+
+        public void MostraResultados()
+        {
+            for(int i = 0; i< resultados.Length; i++)
+            {
+                if(resultados[i] != null)
+                {
+                    Console.WriteLine(resultados[i]);
+                }
+                
+            }
+        }
+
+        public string [] AdicionaArrayResultados(string resultado, int cont)
+        {
+            resultados[cont] = resultado;
+
+            return resultados;
+        }
+
+    }
+}
---
+++ CalculadoraApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora c1 = new Calculadora();
+            int cont = 0;
+            while (true)
+            {
+                c1.MostraMenu();
+
+                c1.operacao = Console.ReadLine();
+                Console.Clear();
+
+                if (c1.operacao.Equals("s", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                if (c1.operacao == "5")
+                {
+                    c1.MostraResultados();
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+
+                if (c1.VerificaOpcaoInvalida(c1.operacao))
+                {
+                    Console.WriteLine("Opção inválida, tente novamente.");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+
+                Console.WriteLine("Digite o primeiro numero: ");
+                c1.numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo numero: ");
+                c1.numero2 = Convert.ToDouble(Console.ReadLine());
+
+                if(c1.operacao == "4" && c1.numero2 == 0){
+                    Console.WriteLine("Não é possível dividir um número por zero.");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+
+                double resultado = 0;
+
+                resultado = c1.RealizaCalculo(c1.operacao, c1.numero1, c1.numero2);
+
+                c1.MostraSimboloOperacao(c1.operacao);
+
+                string mostraResultado = $"Resultado de {c1.numero1} {c1.operacao} {c1.numero2} =  {resultado}";
+
+                c1.AdicionaArrayResultados(mostraResultado, cont);
+                cont++;
+
+                Console.WriteLine(mostraResultado);
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
